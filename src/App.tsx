e1aa8f5f432a292c80e8cf317c87846556caed5c
Oceanteel <oceanteel@proton.me>
2025-06-21 18:26:54 -0400

    Update Hero section with dark blue background

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import Footer from './components/Footer'
 
 function App() {
   return (
-    <div className="min-h-screen bg-slate-800">
+    <div className="min-h-screen" style={{ backgroundColor: '#020f45' }}>
       <Header />
       <Hero />
       <Features />
